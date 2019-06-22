14f55ab3d2847aa3d8aab3f7ef0428aed74ba68e
jamesaberry <jamesaberry@users.noreply.github.com>
2019-06-22 16:50:32 -0400

    Dynamically assign random background image
    Add a randomly selected background
    and sets a style to make use of it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-photo-album';
+  picture = 'https://www.gstatic.com/webp/gallery3/2_webp_ll.png';
+  counter = interval(3000);
+  randNum = 0;
+  currentStyles = {}
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  getImage() {
+    this.randNum = this.getRandomInt(5) + 1;
+    this.picture = 'https://www.gstatic.com/webp/gallery3/' + this.randNum.toString() + '_webp_ll.png';
+    this.currentStyles = {
+      'height':'55em',
+      'background-size':'cover',
+      'width':'auto',
+      'background-position':'50% 50%',
+      'background-image': 'url(' + this.picture + ')' 
+    };
+  }
+  
+  ngOnInit() {
+    this.counter.subscribe( x => 
+      this.getImage()
+      )
+  }
 }
